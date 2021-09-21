ee04883baa75f60ec9cf69f700d611bc1f8e9a15
wolkenTreiberr <ishevchuk1998@gmail.com>
2021-09-21 13:48:31 +0300

    delete Pods

--- android/ios/Pods/Target Support Files/Pods-ReactNativeCLITextProject-ReactNativeCLITextProjectTests/Pods-ReactNativeCLITextProject-ReactNativeCLITextProjectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeCLITextProject_ReactNativeCLITextProjectTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeCLITextProject_ReactNativeCLITextProjectTests
-@end
--- android/ios/Pods/Target Support Files/Pods-ReactNativeCLITextProject/Pods-ReactNativeCLITextProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeCLITextProject : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeCLITextProject
-@end
